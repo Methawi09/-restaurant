9aee2ef8f7963502779b0bfb926c39060949ed09
Methawi09 <86421053+Methawi09@users.noreply.github.com>
2021-12-21 04:13:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,181 +1,167 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <title>Document</title>
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+        
+    <title>carat land</title>
+    <style>
+        .font{
+            font-family: 'PT Sans', sans-serif;
+            font-size: 110%;
+        }
+    </style>
 </head>
-<body>
+
+<body class="font" >
+    <br>
+    
+    <h4 style="text-align: center;">
+        </span>CARAT LAND </h4>
+    <br>
+    <!--แบฟรอม-->
+    <div class="container" style="margin-left: 10%;">
+        <form class="row">
+            <div class="col-1">
+                <input type="hidden" class="form-control" id="Rerow">
+            </div>
+            <div class="col-3">
+                <input type="text" class="form-control" placeholder="Id Food" id="IDFood">
+            </div>
+            <div class="col-3">
+                <input type="text" class="form-control" placeholder="Name Food" id="NameFood">
+            </div>
+            <div class="col-2">
+                <input type="number" class="form-control" placeholder="Price Food" id="PriceFood">
+            </div>
+            <div class="col-1" >
+                <button type="button" class="btn btn-primary " id='btnAdd' style="width: 120%; height: 97%; ">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-plus-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                        <path
+                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                    </svg> Add</button>
+            </div>
+            <div >
+                <button type="button" class="btn btn-info" id='btnUp'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16"
+                        height="16" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                            d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" />
+                    </svg> Up</button>
+            </div>
+            <div style="margin-right: 2%;">
+                <button type="button" class="btn btn-danger" id='btnClear'>
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                        <path
+                            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                        <path fill-rule="evenodd"
+                            d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                    </svg>Clear</button>
+            </div>
+
+
+        </form>
+        <br>
+    </div>
     <div class="container">
-       
-        <div>
-        <table class="table">
+        <table class="table table-hover" id="TableFood" style="width: 85%; margin-left: 6%;">
             <thead>
                 <tr>
-          
-                    <td colspan=2 >
-                    <input type="hidden" name="" id="txtRID" value="0"/>
-                    <input type="password" class="form-control" id="txtUnique" placeholder="Unique Name" />
-                
-                    </td>
-                    <td><input type="text" class="form-control" id="txtName" placeholder="Name" /></td>
-                    <td><input type="text" class="form-control" id="txtOwner" placeholder="Owner" /></td>
-                    <td><input type="text" class="form-control" id="txtWebSite" placeholder="WebSite"/></td>
-                    <td><button class="btn btn-primary"  id="btnAdd"> <i class="fa fa-plus-circle" aria-hidden="true"></i> Add </button></td>
+                    <th scope="col">ID Food</th>
+                    <th scope="col">Name Food</th>
+                    <th scope="col">Price Food</th>
 
-                </tr>
-                <tr>
-                    <th></th>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Owner</th>
-                    <th>Website</th>
-                    <th>Status</th>
                 </tr>
             </thead>
-            <tbody id="tblRestaurants"></tbody>
-       </table>
-        </div>
-      
+            <tbody>
+            </tbody>
+        </table>
 
-        <select id="optCate" class="form-control"> 
-            <option value="1"> Hello </option>
-        </select>
     </div>
-
-
-
-
-
 </body>
 <script>
-    function loadCate(){
-        var url = "https://cjundang.me/api/category/gets";
-        $.getJSON(url)
-            .done((data)=>{
-                var line = "";
-                $.each(data, (k, v)=>{
-                    line += "<option value='" + v.cid + "'> "+ v.categories + "</option>";
-                });
-                $("#optCate").empty();
-                $("#optCate").append(line);
-
-            })
-
-    }
-    function loadRestaurants(){
-        var url = "https://cjundang.me/api/restaurants/gets";
-        $.getJSON(url).done((data)=>{
-            var line = "";
-            console.log(data); 
-            $.each(data, (key, item)=>{
-                    line += "<tr>";
-                    line += "<td> <a class='btn btn-danger btn-sm' href='#' onClick='deleRestaurant("+ item.rid +")'><i class='fa fa-trash' aria-hidden='true'></i> dele </a> ";
-                    line += "<a class='btn btn-primary  btn-sm' href='#' onClick='loadARestaurant("+ item.rid +")' > <i class='fa fa-pencil-square-o' aria-hidden='true'></i> edit </a></td>";
-                    line += "<td>" + item.rid   + "</td>";
-                    line += "<td>" + item.name   + "</td>";
-                    line += "<td>" + item.owner_name   + "</td>";
-                    line += "<td>  <a href='"+ item.website+"'> Link </a></td>";
-                    line += "<td>";
-                    line += "Opened </td>";
-                    line += "</tr>";
-                    console.log(line);
-
-                
-            });
-            $("#tblRestaurants").empty();
-            $("#tblRestaurants").append(line);
-            
-        })
-        .fail((xhr, status, error)=>{
-            
+    $(function () {
+        $('#btnAdd').on('click', function () {
+            var IDFood, NameFood, PriceFood;
+            IDFood = $("#IDFood").val();
+            NameFood = $("#NameFood").val();
+            PriceFood = $("#PriceFood").val();
+            var edit = "<a class='edit' href='JavaScript:void(0);' >Edit</a>";
+            var del = "<a class='delete' href='JavaScript:void(0);'>Delete</a>";
+            if (IDFood == "" || NameFood == "" || PriceFood == " ") {
+                alert("กรุณากรอกข้อมูลให้ครบค่ะ");
+            } else {
+                var table = "<tr><td>" + IDFood + "</td><td>" + NameFood + "</td><td>" + PriceFood + "</td><td>" + edit + "</td><td>" + del + "</td></tr>";
+                $("#TableFood").append(table);
+            }
+            IDFood = $("#IDFood").val("");
+            NameFood = $("#NameFood").val("");
+            PriceFood = $("#PriceFood").val("");
+            Clear();
         });
-    }
-
-    function loadARestaurant(id){
-        var url = "https://cjundang.me/api/restaurants/get/" + id;
-        $.getJSON(url).done((data)=>{
-            console.log(data);
-            var item = data[0];
-            $("#txtRID").val(item.rid);
-            $("#txtName").val(item.name);
-            $("#txtOwner").val(item.owner_name);
-            $("#txtWebSite").val(item.website);
-
-            
-        })
-        .fail((xhr, status, error)=>{
-            
+        $('#btnUp').on('click', function () {
+            var IDFood, NameFood, PriceFood;
+            IDFood = $("#IDFood").val();
+            NameFood = $("#NameFood").val();
+            PriceFood = $("#PriceFood").val();
+            $('#TableFood tbody tr').eq($('#Rerow').val()).find('td').eq(0).html(IDFood);
+            $('#TableFood tbody tr').eq($('#Rerow').val()).find('td').eq(1).html(NameFood);
+            $('#TableFood tbody tr').eq($('#Rerow').val()).find('td').eq(2).html(PriceFood);
+            $('#btnAdd').show();
+            $('#btnUp').hide();
+            Clear();
         });
+        $("#TableFood").on("click", ".delete", function (e) {
+            if (confirm("ยืนยันที่จะการลบรายการอาหาร")) {
+                $(this).closest('tr').remove();
+            } else {
+                e.preventDefault();
+            }
+        });
+        $('#btnClear').on('click', function () {
+            Clear();
+        });
+        $("#TableFood").on("click", ".edit", function (e) {
+            var row = $(this).closest('tr');
+            $('#Rerow').val($(row).index());
+            var td = $(row).find("td");
+            $('#IDFood').val($(td).eq(0).html());
+            $('#NameFood').val($(td).eq(1).html());
+            $('#PriceFood').val($(td).eq(2).html());
+            $('#btnAdd').hide();
+            $('#btnUp').show();
+        });
+
+
+
+    });
+    function Clear(){
+        $("#IDFood").val("");
+        $("#NameFood").val("");
+        $("#PriceFood").val("");
+        $("#Rerow").val("");
     }
 
-    function deleRestaurant(id){
-
-        var unique_name = prompt("confirm to delete with Uniqe Name");
-        if (unique_name != null){
-            var data = new Object();
-            data.unique_name = unique_name;
-            data.rstatus = 1;
-            var url = "https://cjundang.me/api/restaurants/status";
-            $.post(
-                url,
-                { "data" : JSON.stringify(data)},
-                (data, status)=>{
-                    console.log(data + " " + status);
-                    loadRestaurants();
-                } 
-            )
-        }
-
-         
-    }
-
-    function addRestaurant(){
-        var data = new Object();
-        data.unique_name = $("#txtUnique").val();
-        data.name = $("#txtName").val();
-        data.owner_name = $("#txtOwner").val();
-        data.website = $("#txtWebSite").val();
-        console.log(JSON.stringify(data));
-        var rid = parseInt($("#txtRID").val());
-
-        if( rid == 0){  // add
-            var url = "https://cjundang.me/api/restaurants/add";
-            $.post(
-                url,
-                { 
-                    "data" : JSON.stringify(data)
-                },
-                (data, status)=>{
-                    console.log(data + " " + status);
-                    loadRestaurants();
-                } 
-            );
-        }else{
-            var url = "https://cjundang.me/api/restaurants/update";
-            $.post(
-                url,
-                { 
-                    "data" : JSON.stringify(data)
-                },
-                (data, status)=>{
-                    console.log(data + " " + status);
-                    loadRestaurants();
-                } 
-            );
-        }
-    }
-
-    $(()=>{
-         loadRestaurants();
-         loadCate();
-       $("#btnAdd").click(addRestaurant);
-    })
 </script>
-</html> 
+
+</html>
